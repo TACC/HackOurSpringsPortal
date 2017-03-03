e8c03262bf2c75a188423d44878fe796faa956ec
Carrie Arnold <cmarnold@tacc.utexas.edu>
2017-03-03 10:19:31 -0600

    First simple view with a template

--- hackoursprings/hackoursprings/settings.py
+++ hackoursprings/hackoursprings/settings.py
@@ -81,3 +81,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_LOADERS = (
+'django.template.loaders.filesystem.Loader',
+'django.template.loaders.app_directories.Loader',
+#django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_DIRS = (
+    "welldata/templates",
+)
--- hackoursprings/welldata/views.py
+++ hackoursprings/welldata/views.py
@@ -4,4 +4,4 @@ from django.template import loader
 
 def index(request):
     template = loader.get_template('welldata/index.html')
-    return HttpResponse(template.render(context, request))
+    return render(request, 'welldata/index.html', {})
